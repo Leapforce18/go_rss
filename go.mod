eb08f5bd02aeb5ef056b024b3f78bc070d2a4601
caselongo <caselongo@gmail.com>
2020-10-02 13:47:56 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Leapforce-nl/go_rss
+
+go 1.13
---
+++ rss.go
@@ -0,0 +1,29 @@
+package rss
+
+import (
+	"encoding/xml"
+	"net/http"
+
+	rss2_0 "github.com/Leapforce-nl/go_rss/2.0"
+)
+
+// ReadRss2_0 reads rss2.0 feed
+//
+func ReadRss2_0(url string) (*rss2_0.Rss, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	rss := rss2_0.Rss{}
+
+	decoder := xml.NewDecoder(resp.Body)
+	err = decoder.Decode(&rss)
+	if err != nil {
+
+		return nil, err
+	}
+
+	return &rss, nil
+}
